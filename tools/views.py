9bb6dd2df76cbf69c394ea4d02da45a6cf342809
RB3IT <43681720+RB3IT@users.noreply.github.com>
2020-05-07 13:06:16 -0400

    tools_update

--- tools/views.py
+++ tools/views.py
@@ -1,19 +1,23 @@
 from django import http as dhttp
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.conf import settings
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic as dviews
 from django.views.decorators import http as decorators
 
 import base64
 import inflect
+import io
 import json
 import xml.etree.ElementTree as ET
 import math
 import os
 import PIL.Image
 
+from . import models
 from doors import views as doorviews
 from NewDadsDoor import constants
 
@@ -38,35 +42,78 @@ class LengthEstimator(dviews.TemplateView):
     template_name = "tools/lengthestimator.html"
 
 
-SUPPLYFILE = os.path.join(settings.PROJECT_ROOT , "supplies.json")
-SHOPPINGLISTFILE = os.path.join(settings.PROJECT_ROOT , "shoppinglist.html")
-SHOPPINGLISTIMGS = os.path.join(settings.MEDIA_ROOT,"shopping")
-with open(os.path.join(SHOPPINGLISTIMGS,"no image.svg"),'rb') as f:
+class OfficeShoppingList(dviews.TemplateView):
+    """ Shopping List for Office Supplies """
+    template_name = "tools/officesupplies.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        supplies = []
+
+        if (user := self.request.user).is_authenticated:
+            items = models.ShoppingItems.objects.filter(active = True)
+            for item in items:
+                sitem,created = models.ShoppingListItem.objects.get_or_create(user = user, item = item)
+                if created: sitem.save()
+                supplies.append(sitem)
+
+        context['supplies'] = supplies
+        return context
+
+
+@decorators.require_http_methods(["GET",])
+def update_shoppinglist(request):
+    """ Shopping list API """
+    r = request.GET
+
+    if not (user := request.user).is_authenticated:
+        return dhttp.HttpResponse("Unauthorized", status = 401)
+
+    kind = r.get("type",None)
+    name = r.get("name",None)
+    value = r.get("value",None)
+    if kind == "value":
+        try: value = float(value)
+        except: value = None
+    elif kind == "flag":
+        if value not in [True,False]: value = None
+    else:
+        value == None
+    if not name or not isinstance(name,str)\
+        or value is None:
+        return dhttp.HttpResponseBadRequest("Invalid api")
+
+    item = get_object_or_404(models.ShoppingItems, name = name)
+    sitem,created = models.ShoppingListItem.objects.get_or_create(user = user, item = item)
+
+    if kind == "value":
+        sitem.quantity = value
+    else:
+        sitem.reorder_flag = value
+    
+    sitem.save()
+
+    return dhttp.JsonResponse({"success":True})
+
+@decorators.require_http_methods(["GET",])
+def download_shoppinglist(request):
+    """ Returns the current Shopping List cached on the server """
+    if not (user := request.user):
+        return dhttp.HttpResponse("Unauthorized", status = 401)
+
+    file = output_supplies(user)
+    response = dhttp.HttpResponse(file, content_type='application/force-download')
+    response['Content-Disposition'] = f'attachment; filename="Shopping List.html"'
+    return response
+
+with staticfiles_storage.open("tools/shopping/noimage.svg", mode='rb') as f:
     NOIMAGE = base64.b64encode(f.read()).decode("ascii")
 del f
-def load_supplies():
-    """ Loads and returns the local supply file via the json module """
-    with open(SUPPLYFILE, 'r') as f:
-        supplies = json.load(f)
-    for item in supplies['supplies']: item['reorder_quantity'] = float(item['reorder_quantity'])
-    return supplies
 
-def save_supplies(supplies):
-    """ Saves the supply file via the json module """
-    supplies['supplies'] = sorted(supplies['supplies'], key = lambda item: item['name'])
-    with open(SUPPLYFILE,'w') as f:
-        return json.dump(supplies,f)
-
-def output_supplies():
+def output_supplies(user):
     ## Pluralizer
     p = inflect.engine()
-    supplies = load_supplies()['supplies']
-    output = []
-    for item in supplies:
-        if item['quantity'] <= item['reorder_quantity'] or item['reorder_flag']:
-            output.append(item)
-
-    output = sorted(output, key = lambda item: item['name'])
+    reorder = models.ShoppingListItem.objects.filter(user = user, item__active = True, quantity__lte = F("item__reorder_quantity")).order_by("item__name")
+    
     doc = ET.Element("html")
     head = ET.Element("head")
     doc.append(head)
@@ -105,23 +152,20 @@ table{
     header.text = "Shopping List"
     table = ET.Element("table")
     body.append(table)
-    if not output:
+    if not reorder:
         row = ET.Element("tr")
         table.append(row)
 
         out = ET.Element("td", attrib={"class":"noitems","colspan":"2"})
         out.text=u"No Items in Shopping List! 😎 Have a Great Day!"
         row.append(out)
-    for item in output:
+    for sitem in reorder:
         row = ET.Element("tr")
 
         td = ET.Element("td")
         img = ET.Element("img",attrib={"class":"image"})
-        ## .thumbnail files are jpegs, for the record
-        imgpath = os.path.join(SHOPPINGLISTIMGS,item['name']+".thumbnail")
         try:
-            with open(imgpath,'rb') as f:
-                imgdata = base64.b64encode(f.read()).decode('ascii')
+            imgdata = base64.b64encode(sitem.item.thumbnail.read()).decode('ascii')
             imgtype = "jpg"
         except:
             imgdata = NOIMAGE
@@ -131,76 +175,21 @@ table{
         row.append(td)
         
         name = ET.Element("td",attrib={"class":"item"})
-        name.text = item['name']
+        name.text = sitem.item.name
         row.append(name)
 
         quantcol = ET.Element("td",attrib={"class":"quant"})
-        orderquant = math.ceil(item['max_quantity']-item['quantity'])
-        quantcol.text = f"{orderquant} {p.plural(item['unit'],orderquant)}"
+        orderquant = math.ceil(sitem.item.max_quantity-sitem.quantity)
+        quantcol.text = f"{orderquant} {p.plural(sitem.item.unit,orderquant)}"
         row.append(quantcol)
 
         table.append(row)
 
-    with open(SHOPPINGLISTFILE,'w',encoding="utf-8") as f:
-        f.write("""<!DOCTYPE html>""")
-        ET.ElementTree(doc).write(f,xml_declaration=False,encoding="unicode")
-
-class OfficeShoppingList(dviews.TemplateView):
-    """ Shopping List for Office Supplies """
-    template_name = "tools/officesupplies.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        supplies = load_supplies()
-        context['supplies'] = supplies['supplies']
-        return context
-
-   
-@decorators.require_http_methods(["GET",])
-def update_shoppinglist(request):
-    """ Shopping list API """
-    r = request.GET
-
-    kind = r.get("type",None)
-    name = r.get("name",None)
-    value = json.loads(r.get("value",None))
-    if kind == "value":
-        try: value = float(value)
-        except: value = None
-    elif kind == "flag":
-        if value not in [True,False]: value = None
-    else:
-        value == None
-    if not name or not isinstance(name,str)\
-        or value is None:
-        return dhttp.HttpResponseBadRequest("Invalid api")
-
-    supplies = load_supplies()
-    for item in supplies['supplies']:
-        if item['name'] == name:
-            name = item
-            break
-    if not isinstance(name,dict):
-        return dhttp.HttpResponseBadRequest("Invalid api")
-
-    if kind == "value":
-        item['quantity'] = value
-    else:
-        item['reorder_flag'] = value
-    
-    save_supplies(supplies)
-    output_supplies()
-    return dhttp.JsonResponse({"success":True})
-
-@decorators.require_http_methods(["GET",])
-def download_shoppinglist(request):
-    """ Returns the current Shopping List cached on the server """
-    with open(SHOPPINGLISTFILE,'r') as f:
-        file = f.read()
-    response = dhttp.HttpResponse(file, content_type='application/force-download')
-    response['Content-Disposition'] = f'attachment; filename="Shopping List.html"'
-    return response
-
+    f = io.StringIO()
+    f.write("""<!DOCTYPE html>""")
+    ET.ElementTree(doc).write(f,xml_declaration=False,encoding="unicode")
+    f.seek(0)
+    return f
 
 class SpringsTool(dviews.TemplateView):
     """ Shopping List for Office Supplies """
